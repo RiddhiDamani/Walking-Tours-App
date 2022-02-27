b6e8a07dcd56adc6044ad9edbfc23fce7ea001c6
Riddhi Damani <riddhidamani90@gmail.com>
2022-02-27 13:35:15 -0600

    Added  Splash, Maps, Building Feature Templates

---
+++ app/src/main/java/com/riddhidamani/walkingtourapp/FeatureBuildingActivity.java
@@ -0,0 +1,14 @@
+package com.riddhidamani.walkingtourapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeatureBuildingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feature_building);
+    }
+}
